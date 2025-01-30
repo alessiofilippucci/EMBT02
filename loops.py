1decfab11906ae1a586c0deb7035819c9a6e50d7
Alessio Filippucci <alessio.filippucci@digitware.it>
2025-01-30 11:34:01 +0100

    test

--- loops.py
+++ loops.py
@@ -1,17 +1,25 @@
-list1 = ['A', 'B', 23, True, "Ciao"]
+# list1 = ['A', 'B', 23, True, "Ciao"]
 
-for listItem in list1:
-    print(listItem)
+# for listItem in list1:
+#     print(listItem)
 
 
-for i in range(10):
-    print(i)
+# for i in range(10):
+#     print(i)
 
-num = 0
+# num = 0
 
-while(num < 10):
-    print(num)
-    num +=1
+# while(num < 10):
+#     print(num)
+#     num +=1
 
-while(True):
-    print("Infinte loop")
+# while(True):
+#     print("Infinte loop")
+
+allowedOps = ["+", "-", "/", "*",]
+op = input(f"Scegli l'operazione. Scegli tra: {allowedOps}")
+
+if op in allowedOps:
+    print("Operazione scelta valida")
+else:
+    print("Operazione scelta non valida")
