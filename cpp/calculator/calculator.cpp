6dc806b7bb585cec54d43c9ea9fa4f55cd34f0cc
Alessio Filippucci <alessio.filippucci@digitware.it>
2025-02-13 11:16:38 +0100

    Add calc

---
+++ cpp/calculator/calculator.cpp
@@ -0,0 +1,69 @@
+#include <iostream> // Including the input-output stream header file
+
+using namespace std; // Using the standard namespace
+
+int main() // Start of the main function
+{
+    int a, b; // Declaring integer variables width, length, area, and perimeter
+
+    cout << "Calculator\n";                             // Outputting a message indicating program name
+    cout << "--------------------------------------\n\n"; // Outputting a separator line
+
+    cout << "Enter first number: ";            // Prompting the user to input the first number
+    cin >> a;                                    // Taking input for the first number
+
+    cout << "\nEnter second number: ";            // Prompting the user to input the first number
+    cin >> b;                                    // Taking input for the first number
+
+    int choice;
+
+    // Display the menu
+
+    cout << "Press 1 to calculate Sum of Numbers\n";
+    cout << "Press 2 to calculate Difference of Numbers\n";
+    cout << "Press 3 to calculate Product of numbers\n";
+    cout << "Press 4 to calculate Division of numbers\n";
+    cout << "Press 5 to exit\n";
+
+    // Enter the choice
+    cout << "Enter your choice:\n";
+    choice = 1;
+    cout << "Choice is " << choice << endl;
+
+    // Display the result
+    // according to the choice
+
+    // Display the result
+    switch (choice)
+    {
+    case 1:
+    {
+        cout << "Sum is " << (a + b) << "\n";
+        break;
+    }
+    case 2:
+    {
+        cout << "Difference is " << (a - b) << "\n";
+        break;
+    }
+    case 3:
+    {
+        cout << "Product is " << (a * b) << "\n";
+        break;
+    }
+    case 4:
+    {
+        cout << "Division is " << (a / b) << "\n";
+        break;
+    }
+    case 7:
+    {
+        cout << "Thank you\n";
+        break;
+    }
+    default:
+        cout << "Wrong Input\n";
+    }
+
+    return 0; // Returning 0 to indicate successful program execution
+} // End of the main function
---
+++ cpp/calculator/calculator.md
@@ -0,0 +1,15 @@
+Write a menu-driven program using the Switch case to calculate the following
+
+Sample Output:
+
+Enter first number: 5
+Enter second number: 7
+
+Press 1 to calculate Sum of Numbers
+Press 2 to calculate Difference of Numbers
+Press 3 to calculate Product of numbers
+Press 4 to calculate Division of numbers
+
+Enter your choice:
+Choice is 1
+Sum is 12
